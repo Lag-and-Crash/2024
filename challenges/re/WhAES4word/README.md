62ab8ea3396a33ef2fdb7a5df380daddb9d8491f
Lee Zhi Wei <zhiweilee.com@gmail.com>
2024-01-19 13:22:22 +0800

    Dumping Ground

---
+++ challenges/re/WhAES4word/README.md
@@ -0,0 +1,28 @@
+# WhAES4Word
+## Author
+[leezhiwei](https://github.com/leezhiwei)
+## Category
+Reverse Engineering
+## Description
+```Hey you, I have a creepy coworker, I think I found his server online... He has an unknown EXE on there, wonder what it does... Can you help me see what it is? The original name is "AEScited.exe"```
+## Difficulty
+Hard
+## Discord
+```lee8219```
+## Files
+- dist/WhatIsThis.exe
+## Flag
+```LNC24{H311oY0Uf0undMyS3creTL1ttl3B0y}```  
+## Hints
+- Cost : 200
+- ```Hey, I did some analysis, this seems to be using some sort of CIL (Common Intermediate Language)```
+- Cost : 300
+- ```Hey, from what I seem you can take a PEEK on this EXE using a took (hint hint Google .NET Peek)```
+- Cost : 400
+- ```Lastly, dive into the source code, what have my colleague coded... What is he doing?```
+- Cost : 500
+- ```P.S look at what the Root Namespace calls. (Piece them together)```
+## Challenge Name
+WhAES4Word
+## C# Source Code
+```solutions/solution_files/WhatIsThis```
---
+++ challenges/re/WhAES4word/solution/solution_files/WhatIsThis/WhatIsThis/SubClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsThis
+{
+    class SubClass : SuperClass
+    {
+        private List<Byte> kee = new List<Byte>
+            { 0x54, 0x68, 0x69, 0x73, 0x49, 0x73, 0x41, 0x4C, 0x65, 0x65, 0x74, 0x4B, 0x65, 0x79, 0x21, 0x21 };
+        public ulong What(int x)
+        {
+            var y = 0;
+            while (x + y == y + x + x)
+            {
+                y++;
+                x++;
+                y++;
+                x++;
+                y++;
+                What(x);
+            }
+
+            return 12345;
+        }
+        public SubClass() : base() { }
+        public override int Funct1()
+        {
+            string h = "";
+            List<char> c = new List<char> { 'a', 'b', 'c', '!', '1', '2' };
+            foreach (char x in c)
+            {
+                h += x;
+            }
+            return base.Return0(h);
+        }
+
+        public override SuperClass Whatisthis()
+        {
+            SubClass x = new SubClass();
+            x.doubl = 1;
+            x.What(1);
+            x.What(Convert.ToInt32(Convert.ToDouble(123456789)));
+            return this;
+        }
+
+        public override string Part1OfFlag(int yy)
+        {
+            return "b";
+        }
+
+        public int veryembmethod(int x, int y, int z, int xyz)
+        {
+            if (x == y && y == z && z == xyz)
+            {
+                return 1;
+            }
+            else if (x ==11)
+            {
+                return 22;
+            }
+            else if (y == 987)
+            {
+                return 2222;
+            }
+            else
+            {
+                return 100000;
+            }
+        }
+
+        public string theb(int x)
+        {
+            return x + "c";
+        }
+    }
+}
