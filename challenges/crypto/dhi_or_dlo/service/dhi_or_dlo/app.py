0d65e22780e5129683dd27fb7444b90b3c3ba110
Warriii <110900380+Warriii@users.noreply.github.com>
2024-01-19 19:49:47 +0800

    Add files via upload

--- challenges/crypto/dhi_or_dlo/service/dhi_or_dlo/app.py
+++ challenges/crypto/dhi_or_dlo/service/dhi_or_dlo/app.py
@@ -3,12 +3,12 @@ from math import gcd
 from secret import flag
 from Crypto.Util.number import bytes_to_long
 
-NB = 700
+r = 3**333
 m = bytes_to_long(flag)
 while True:
     rsa = RSA.generate(2048)
     n, p, q = rsa.n, rsa.p, rsa.q
-    e = 17
+    e = 5
     if gcd(e, (p-1)*(q-1)) == 1:
         break
 d = pow(e, -1, (p-1)*(q-1))
@@ -17,12 +17,12 @@ c = pow(m, e, n)
 print(f"n = {n}")
 print(f"e = {e}")
 print(f"c = {c}")
+print(f"r = {r}")
 print("dhi or dlo?")
 userinput = str(input(">> "))
 if userinput == "dhi":
-    print(f"dhi = {d // 2**(2048 - NB)}")
+    print(f"dhi = {d // 3**(1293 - 333)}")
 elif userinput == "dlo":
-    print(f"dlo = {d % 2**NB}")
+    print(f"dlo = {d % r}")
 else:
     print("Invalid input.")
-
