8743413b5e0f72e3e10e3668ee7ac45480aa1eeb
Warriii <110900380+Warriii@users.noreply.github.com>
2024-01-23 15:03:31 +0800

    Crypto Hard chall submission!
    Knapsack cryptography, fun with lattices
    Rated Hard as I think you could solve this with the dynamic programming knapsack algo
    (not sure if 2**48 brute force is viable)

---
+++ challenges/crypto/Backpacker/dist/chall.py
@@ -0,0 +1,35 @@
+from secret import flag
+from random import randint
+from Crypto.Util.number import isPrime
+from math import gcd
+
+def gen_pub(n):
+    W = []
+    s = 0
+    while len(W) < n:
+        w = s + randint(2, 1337)
+        W.append(w)
+        s += w
+    q = s*2
+    while not isPrime(q):
+        q = randint(2*s, 3*s)
+    r = randint(2, q)
+    assert gcd(r,q) == 1
+    pub = [r*w % q for w in W]
+    return pub
+
+assert len(flag) == 24
+for i in range(0, len(flag), 6):
+
+    fblock = flag[i:i+6]
+    pub = gen_pub(len(fblock)*8)
+    ct = 0
+    m = int.from_bytes(fblock, "big")
+
+    cnt = 0
+    while m:
+        ct += (m & 1) * pub[cnt]
+        cnt += 1
+        m >>= 1
+    print(f"pub{i//6} = {pub}")
+    print(f"c{i//6} = {ct}")
